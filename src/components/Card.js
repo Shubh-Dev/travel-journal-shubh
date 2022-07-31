64a5a10a1c11f150cdacbf8705a480f4b37d02c5
Shubh <shubh.scb@gmail.com>
2022-08-01 00:18:01 +0530

    DATA MAPPED

---
+++ src/components/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Card = (props) => {
+  console.log(props);
+  return (
+    <div className="card">
+      <div className="image-holder">
+        <img src={props.imageUrl} alt="image" className="card-img" />
+      </div>
+
+      <div className="card-info">
+        <div className="maps-division">
+          <div className="pinpoint-location-icon">
+            <img
+              src="../images/pinpoint.svg"
+              alt="maps"
+              className="maps-icon"
+            />
+            <h6>{props.location}</h6>
+          </div>
+          <a
+            href={props.googleMapsUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="map-link"
+          >
+            <p>view on google map</p>
+          </a>
+        </div>
+        <h2>{props.title}</h2>
+        <div>
+          <p>
+            {props.startDate}-{props.endDate}
+          </p>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <img src="../images/globe.svg" className="globe-icon" alt="globe" />
+    <h5 className="heading">my travel journal</h5>
+  </div>
+);
+
+export default Header;
